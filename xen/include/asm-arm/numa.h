f1de3e5a43cdcdc9599e10277b725177c1b684bd
Vijaya Kumar K <Vijaya.Kumar@cavium.com>
2017-01-20 02:32:00 -0500

    ARM: NUMA: Add existing ARM numa code under CONFIG_NUMA
    Right now CONFIG_NUMA is not enabled for ARM and
    existing code in asm-arm/numa.h is for !CONFIG_NUMA.
    Hence put this code under #ifndef CONFIG_NUMA.
    
    This help to make this changes work when CONFIG_NUMA
    is not enabled. Though CONFIG_NUMA is enabled by default,
    manually disabling this option is possible and compilation
    should go through. Hence kept the these changes under
    !CONFIG_NUMA.
    
    Signed-off-by: Vijaya Kumar K <Vijaya.Kumar@cavium.com>
    ---
    v3: - Dropped NODE_SHIFT define

--- xen/include/asm-arm/numa.h
+++ xen/include/asm-arm/numa.h
@@ -3,6 +3,7 @@
 
 typedef uint8_t nodeid_t;
 
+#ifndef CONFIG_NUMA
 /* Fake one node for now. See also node_online_map. */
 #define cpu_to_node(cpu) 0
 #define node_to_cpumask(node)   (cpu_online_map)
@@ -16,6 +17,7 @@ static inline __attribute__((pure)) nodeid_t phys_to_nid(paddr_t addr)
 #define node_spanned_pages(nid) (total_pages)
 #define node_start_pfn(nid) (pdx_to_pfn(frametable_base_pdx))
 #define __node_distance(a, b) (20)
+#endif /* CONFIG_NUMA */
 
 static inline unsigned int arch_get_dma_bitsize(void)
 {
